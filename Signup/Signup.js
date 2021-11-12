ec5517133196bbf4b61aeeb844a1b28dd43d9417
Aswin ES <aswinedassery@gmail.com>
2021-11-12 20:50:52 +0530

    form validaton done

---
+++ Signup/Signup.js
@@ -0,0 +1,77 @@
+//Input data
+let email= document.getElementById("inputEmail4")
+let password= document.getElementById("inputPassword4")
+let confirm= document.getElementById("confirmPassword")
+let phoneno= document.getElementById("phoneno")
+
+//error
+let Eerr=document.getElementsByClassName("Eerr")
+let vis= document.getElementsByClassName("vis")
+
+function validate(){
+    let regexemail=/^([A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/;
+    let regexpasswrd=/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    let phnno=/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/;
+    let e,p,c,no;
+    if(regexemail.test(email.value.trim())){
+        e=true;
+        vis[0].style.visibility="visible";
+        Eerr[0].style.visibility='hidden';
+        Eerr[1].style.visibility='hidden';
+
+      }else{
+        Eerr[0].style.visibility='visible';
+        Eerr[1].style.visibility='visible';
+        vis[0].style.visibility="hidden";
+     
+        }
+    if(regexpasswrd.test(password.value)){
+        p = true;
+        vis[1].style.visibility="visible";
+        Eerr[2].style.visibility='hidden';
+        Eerr[3].style.visibility='hidden';
+
+    }else{
+        Eerr[3].style.visibility='visible';
+        Eerr[2].style.visibility='visible';
+        vis[1].style.visibility="hidden";
+       
+    }
+    if(password.value===''){
+        Eerr[4].style.visibility='visible';
+        Eerr[5].style.visibility='visible';
+        vis[2].style.visibility="hidden";
+       
+
+    }else if(password.value===confirm.value){
+        c = true;
+        vis[2].style.visibility="visible";
+        Eerr[4].style.visibility='hidden';
+        Eerr[5].style.visibility='hidden';
+
+    }else{
+        Eerr[4].style.visibility='visible';
+        Eerr[5].style.visibility='visible';
+        vis[2].style.visibility="hidden";
+       
+    
+    }
+    if(phnno.test(phoneno.value)){
+        no = true;
+        vis[3].style.visibility="visible";
+        Eerr[6].style.visibility='hidden';
+        Eerr[7].style.visibility='hidden';
+
+    }else{
+        vis[3].style.visibility="hidden";
+        Eerr[6].style.visibility='visible';
+        Eerr[7].style.visibility='visible';
+    }
+
+    if(e===true && p===true && c===true && no===true){
+        return true;
+    }else{
+        return false;
+    }
+        
+}
---
+++ Signup/check.js
@@ -0,0 +1,28 @@
+let pass1=document.getElementById('inputPassword4')
+let space=document.getElementById('checkspace')
+
+ let strongPassword = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})')
+    let mediumPassword = new RegExp('((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))')
+
+
+
+function strength(){
+   
+    if(strongPassword.test(pass1.value)) {
+            space.style.backgroundColor = 'green'
+            // space.innerHTML= 'Strong'
+            space.style.width = '80%'
+
+            
+        } else if(mediumPassword.test(pass1.value)){
+            space.style.backgroundColor = 'orange'
+            // space.innerHTML = 'Medium'
+            space.style.width = "52%"
+
+        } else{
+            space.style.backgroundColor = 'red'
+            // space.innerHTML = 'Weak'
+            space.style.width = "26%"
+        }
+
+}
